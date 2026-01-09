8b2a97f472492fa4d2949d70a875e50d0fecdf05
Victor de Souza Magaldi <victmagaldi@hotmail.com>
2026-01-08 22:20:51 -0300

    chore: Configure prisma 7 in project

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,13 @@
+import 'dotenv/config'
 import fastify from 'fastify'
+import { PrismaPg } from '@prisma/adapter-pg'
+import { PrismaClient } from '../generated/prisma/client'
 
+const connectionString = `${process.env.DATABASE_URL}`
+
+const adapter = new PrismaPg({ connectionString })
+const prisma = new PrismaClient({ adapter })
+
+export { prisma }
+console.log('🚀 ~ prisma:', prisma)
 export const app = fastify()

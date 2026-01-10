8f01890fd4996bc199d7f5f4e6eabdb52993d515
Victor de Souza Magaldi <victmagaldi@hotmail.com>
2026-01-10 20:08:11 -0300

    feat: create first useCase

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,7 +1,7 @@
-import { prisma } from '@/src/lib/prisma'
-import { hash } from 'bcryptjs'
 import { FastifyReply, FastifyRequest } from 'fastify'
 import z from 'zod'
+
+import { registerUseCase } from '@/src/use-cases/register'
 
 export async function register(request: FastifyRequest, reply: FastifyReply) {
   const registerSchema = z.object({
@@ -11,19 +11,11 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
   })
 
   const { name, email, password } = registerSchema.parse(request.body)
-  const password_hash = await hash(password, 6)
-
-  const userWithSameEmail = await prisma.user.findUnique({
-    where: {
-      email,
-    },
-  })
-
-  if (userWithSameEmail) {
+  try {
+    await registerUseCase({ name, email, password })
+  } catch (error) {
     return reply.status(409).send()
   }
-
-  await prisma.user.create({ data: { email, name, password_hash } })
 
   return reply.status(201).send()
 }

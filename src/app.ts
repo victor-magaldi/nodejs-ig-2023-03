049778cbd73eac069e40bc0da5595e51c3a2ee49
Victor de Souza Magaldi <victmagaldi@hotmail.com>
2026-01-10 21:28:31 -0300

    feat: add handle error global

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,23 @@
 import 'dotenv/config'
 import fastify from 'fastify'
 import { appRoutes } from './http/routes'
+import { env } from '@/env'
+import { z, ZodError } from 'zod'
 
 export const app = fastify()
 app.register(appRoutes)
+app.setErrorHandler((error, _, reply) => {
+  if (error instanceof ZodError) {
+    return reply
+      .status(400)
+      .send({ message: 'Validation error.', issues: z.treeifyError(error) })
+  }
+
+  if (env.NODE_ENV !== 'production') {
+    console.error(error)
+  } else {
+    // TODO: Here we should log to a external tool like DataDog/NewRelic/Sentry
+  }
+
+  return reply.status(500).send({ message: 'Internal server error.' })
+})
--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -22,7 +22,7 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
     if (error instanceof UserAlreadyExistsError) {
       return reply.status(409).send({ error: error.message })
     }
-    return reply.status(500).send()
+    throw error
   }
 
   return reply.status(201).send()

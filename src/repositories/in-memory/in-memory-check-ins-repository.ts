037afc14110f6c1db933df82df15a9e014dd4035
Victor de Souza Magaldi <victmagaldi@hotmail.com>
2026-01-13 19:52:49 -0300

    feat: init TDD in check-in

--- src/repositories/in-memory/in-memory-check-ins-repository.ts
+++ src/repositories/in-memory/in-memory-check-ins-repository.ts
@@ -6,6 +6,22 @@ import { CheckInsRepository } from '@/src/repositories/check-ins-repository'
 export class InMemoryCheckInsRepository implements CheckInsRepository {
   public items: CheckIn[] = []
 
+  async findByUserIdOnDate(userId: string, date: Date) {
+    console.log(
+      '🚀 ~ InMemoryCheckInsRepository ~ findByUserIdOnDate ~ date:',
+      date,
+    )
+    const checkInOnSameDate = this.items.find(
+      (checkIn) => checkIn.user_id === userId,
+    )
+
+    if (!checkInOnSameDate) {
+      return null
+    }
+
+    return checkInOnSameDate
+  }
+
   async create(data: Prisma.CheckInUncheckedCreateInput) {
     const checkIn = {
       id: randomUUID(),

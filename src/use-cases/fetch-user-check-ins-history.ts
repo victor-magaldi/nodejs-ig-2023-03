3064bbeee28cbbb6aba0248d43b0110c88d4e7e4
Victor de Souza Magaldi <victmagaldi@hotmail.com>
2026-01-13 21:36:52 -0300

    feat: implment use case check-in paginate

---
+++ src/use-cases/fetch-user-check-ins-history.ts
@@ -0,0 +1,29 @@
+import { CheckInsRepository } from '@/src/repositories/check-ins-repository'
+import { CheckIn } from '@/generated/prisma/client'
+
+interface FetchUserCheckInsHistoryUseCaseRequest {
+  userId: string
+  page: number
+}
+
+interface FetchUserCheckInsHistoryUseCaseResponse {
+  checkIns: CheckIn[]
+}
+
+export class FetchUserCheckInsHistoryUseCase {
+  constructor(private checkInsRepository: CheckInsRepository) {}
+
+  async execute({
+    userId,
+    page,
+  }: FetchUserCheckInsHistoryUseCaseRequest): Promise<FetchUserCheckInsHistoryUseCaseResponse> {
+    const checkIns = await this.checkInsRepository.findManyByUserId(
+      userId,
+      page,
+    )
+
+    return {
+      checkIns,
+    }
+  }
+}

ba2a9bcbaf4da7cd448e52d5a1d9af4fba1ac6c4
Victor de Souza Magaldi <victmagaldi@hotmail.com>
2026-01-13 21:29:41 -0300

    feat: add use cases for gym  register and improve errors distance

--- src/repositories/in-memory/in-memory-gyms-repository.ts
+++ src/repositories/in-memory/in-memory-gyms-repository.ts
@@ -1,4 +1,6 @@
-import { Gym } from '@/generated/prisma/client'
+import { randomUUID } from 'node:crypto'
+
+import { Gym, Prisma } from '@/generated/prisma/client'
 import { GymsRepository } from '@/src/repositories/gyms-repository'
 
 export class InMemoryGymsRepository implements GymsRepository {
@@ -10,6 +12,22 @@ export class InMemoryGymsRepository implements GymsRepository {
     if (!gym) {
       return null
     }
+
+    return gym
+  }
+
+  async create(data: Prisma.GymCreateInput) {
+    const gym = {
+      id: data.id ?? randomUUID(),
+      title: data.title,
+      description: data.description ?? null,
+      phone: data.phone ?? null,
+      latitude: new Prisma.Decimal(data.latitude.toString()),
+      longitude: new Prisma.Decimal(data.longitude.toString()),
+      created_at: new Date(),
+    }
+
+    this.items.push(gym)
 
     return gym
   }

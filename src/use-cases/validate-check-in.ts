0f0793370e036ec6df8c0e1d4fd0a719ee6757fc
Victor de Souza Magaldi <victmagaldi@hotmail.com>
2026-01-14 21:05:14 -0300

    feat: implement use case for validate Checkin

---
+++ src/use-cases/validate-check-in.ts
@@ -0,0 +1,33 @@
+import { CheckIn } from '@/generated/prisma/client'
+import { CheckInsRepository } from '@/src/repositories/check-ins-repository'
+import { ResourceNotFoundError } from '@/src/use-cases/errors/resource-not-found-error'
+
+interface ValidateCheckInUseCaseRequest {
+  checkInId: string
+}
+
+interface ValidateCheckInUseCaseResponse {
+  checkIn: CheckIn
+}
+
+export class ValidateCheckInUseCase {
+  constructor(private checkInsRepository: CheckInsRepository) {}
+
+  async execute({
+    checkInId,
+  }: ValidateCheckInUseCaseRequest): Promise<ValidateCheckInUseCaseResponse> {
+    const checkIn = await this.checkInsRepository.findById(checkInId)
+
+    if (!checkIn) {
+      throw new ResourceNotFoundError()
+    }
+
+    checkIn.validated_at = new Date()
+
+    await this.checkInsRepository.save(checkIn)
+
+    return {
+      checkIn,
+    }
+  }
+}

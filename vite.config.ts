9b74458461c07833f343e16362c33aa5d3ae44e7
Victor de Souza Magaldi <victmagaldi@hotmail.com>
2026-01-24 22:46:40 -0300

    chore: configure test e2e (middlwares)

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,23 @@ import tsConfigPaths from 'vite-tsconfig-paths'
 export default defineConfig({
   plugins: [tsConfigPaths()],
   test: {
-    coverage: {
-      include: ['src/**/*.{ts,tsx}'],
-    },
+    dir: 'src',
+    projects: [
+      {
+        extends: true,
+        test: {
+          name: 'e2e',
+          dir: 'src/http/controllers',
+          environment: `./prisma/vitest-environment/prisma-test-environment.ts`,
+        },
+      },
+      {
+        extends: true,
+        test: {
+          name: 'unit',
+          dir: 'src/use-cases',
+        },
+      },
+    ],
   },
 })

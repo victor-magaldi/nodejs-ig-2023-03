5a060657f22778dd6bc10a6a27029a9f825d8693
Victor de Souza Magaldi <victmagaldi@hotmail.com>
2026-01-24 22:54:36 -0300

    feat: implment test e2e register

--- src/http/controllers/register.spec.ts
+++ src/http/controllers/register.spec.ts
@@ -1,5 +1,23 @@
-import { expect, test } from 'vitest'
+import request from 'supertest'
+import { app } from '@/src/app'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
 
-test('testing', () => {
-  expect(1 + 1).equal(2)
+describe('Register (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to register', async () => {
+    const response = await request(app.server).post('/users').send({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456',
+    })
+
+    expect(response.statusCode).toEqual(201)
+  })
 })

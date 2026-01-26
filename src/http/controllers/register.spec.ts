88684882a7179a48dfba3fcde9b5837b9e400126
Victor de Souza Magaldi <victmagaldi@hotmail.com>
2026-01-25 21:04:38 -0300

    test: implement e2e profile

--- src/http/controllers/register.spec.ts
+++ src/http/controllers/register.spec.ts
@@ -14,7 +14,7 @@ describe('Register (e2e)', () => {
   it('should be able to register', async () => {
     const response = await request(app.server).post('/users').send({
       name: 'John Doe',
-      email: 'johndoe@example.com',
+      email: 'johndoeregister@example.com',
       password: '123456',
     })
 
